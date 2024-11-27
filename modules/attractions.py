02421a63fc4715e3cd1fbb8bf192365a1610dfa8
evangelostsak <baggtsak55@gmail.com>
2024-11-27 13:09:37 +0100

    better comments

--- modules/attractions.py
+++ modules/attractions.py
@@ -15,7 +15,17 @@ def get_api_key():
 def fetch_attractions(lat, long, radius, attr_type, api_key):
     """Fetch desired attraction creating a unique request
        regarding users likes
-        """
+
+    Parameters:
+        lat (float): Latitude of the location.
+        long (float): Longitude of the location.
+        radius (int): Radius for the search in meters.
+        attr_type (str): Category of the attraction.
+        api_key (str):API key.
+
+    Returns:
+        list: List of attractions returned by the API.
+    """
     url = "https://api.geoapify.com/v2/places"
     params = {
         "categories": attr_type,  # e.g. tourism.attraction or 1.2 in screenshot
