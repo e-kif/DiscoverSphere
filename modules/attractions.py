88d79173eb1f222f56e8e2d9c9d74526d9401736
evangelostsak <baggtsak55@gmail.com>
2024-11-27 09:38:11 +0100

    api_key init

--- modules/attractions.py
+++ modules/attractions.py
@@ -1,4 +1,22 @@
-# fetch attraction places from api given location and attraction type
-# https://apidocs.geoapify.com/playground/places/
+from dotenv import load_dotenv
+import requests
+import os
+from requests.structures import CaseInsensitiveDict
+load_dotenv()
 
-# test
+
+def get_api_key():
+    """Gets API key from the environment."""
+    api_key = os.getenv("API_KEY")
+    if not api_key:
+        raise ValueError("API key not found. Please check the .env file.")
+    return api_key
+
+
+def fetch_attractions():
+    pass
+
+    #  fetch attraction places from api given location and attraction type
+    #  https://apidocs.geoapify.com/playground/places/
+
+    # test
