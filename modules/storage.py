f699843763f1e5165c5ab5d6bb7128a2e08b58b5
e-kif <e.kifiuk@gmail.com>
2024-11-27 11:08:47 +0200

    module storage was initiated

---
+++ modules/storage.py
@@ -0,0 +1,9 @@
+"""
+this module should keep track of our users.
+we should save
+- user phone number
+- last selected location
+- last selected type of attraction
+- list of attractions
+- last attraction that we sent to the user
+"""
