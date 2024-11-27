d01343f2a4d56b0dc88d3e299a00e152131a135f
e-kif <e.kifiuk@gmail.com>
2024-11-27 22:36:36 +0200

    welcome sms for new subscribers

--- modules/user_interaction.py
+++ modules/user_interaction.py
@@ -1,33 +1,35 @@
+from dotenv import load_dotenv
 from datetime import datetime
 import os
 
 from modules.messages_manager import register_number, unregister_number, read_messages, send_message
 
-
+load_dotenv()
+some_phone_number = int(os.getenv('some_number'))
 sms_commands = {}
 TEAM_NAME = 'Attraction'
 
 
 def add_log_record(status: int, record: str, log_filename: str = os.path.join('..', 'storage', 'app.log')):
-    if os.path.exists(log_filename):
-        with open(log_filename, 'a', encoding='utf8') as log:
-            log.write(f'{datetime.now()}\t{status} {record}\n')
-    else:
-        with open(log_filename, 'w+', encoding='utf8') as log:
-            log.write(f'{datetime.now()}\t{status} {record}\n')
+    with open(log_filename, 'a', encoding='utf8') as log:
+        log.write(f'{datetime.now()}\t{status} {record}\n')
 
 
-def subscribe_user(user_number: int, text: str, team: str = TEAM_NAME) -> int:
+def subscribe_user(user_number: int, text: str = '', team: str = TEAM_NAME) -> int:
     add_log_record(100, f'Subscribing number {user_number} to {text} group')
-    # code, message = register_number(user_number)
-    # if code == 200:
-    #     add_log_record(code, f'User {user_number} was successfully unsubscribed from {team}')
-    #     TODO send welcome sms
-    #     TODO add new user to persistent storage
-    #     return code
-    # add_log_record(code, message)
-    # return code
-    pass
+    code, message = register_number(user_number)
+    if code == 200:
+        add_log_record(code, f'User {user_number} was successfully subscribed for {team}')
+        loc_keyword = list(sms_commands.keys())[2]
+        welcome_text = ('Welcome to the DiscoverSphere app!\n'
+                        f'To set your destination reply to this SMS with keyword {loc_keyword} '
+                        f'followed by a city. Example:\n{loc_keyword} Berlin')
+        sms_code, sms_message = send_message(user_number, welcome_text)
+        add_log_record(sms_code, sms_message)
+        # TODO add new user to persistent storage
+        return sms_code
+    add_log_record(code, message)
+    return code
 
 
 def unsubscribe_user(user_number: int, text: str, team: str = TEAM_NAME) -> int:
@@ -61,17 +63,17 @@ def send_documentation():
 
 
 sms_commands.update({
-    'SUBSCRIBE': subscribe_user,
-    'UNSUBSCRIBE': unsubscribe_user,
-    'LOCATION': set_location,
-    'TYPE': set_attraction_type,
-    'MORE': send_next_attraction,
-    'UNSUPPORTED': send_documentation
+    'SUBSCRIBE': ['SUBSCRIBE', subscribe_user],
+    'UNSUBSCRIBE': ['UNSUBSCRIBE', unsubscribe_user],
+    'LOCATION': ['LOCATION', set_location],
+    'TYPE': ['TYPE', set_attraction_type],
+    'MORE': ['MORE', send_next_attraction],
+    'DOCS': ['DOCS', send_documentation]
 })
 
 
 def main():
-    add_log_record(200, 'Log test')
+    subscribe_user(some_phone_number)
 
 
 if __name__ == '__main__':
