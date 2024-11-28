cc79163a5df66561870a7e4031cf37bd0ad06bd1
e-kif <e.kifiuk@gmail.com>
2024-11-28 02:27:01 +0200

    user registering and unregistering were implemented

--- modules/sms_builder.py
+++ modules/sms_builder.py
@@ -1,10 +1,38 @@
 import os
 from dotenv import load_dotenv
+import json
 from random import randint
 
 
-def get_random_city():
-    cities = os.getenv('cities').split(', ')
-    index = randint(0, len(cities) - 1)
-    return cities[index]
+load_dotenv()
 
+
+def get_random_item(env_key: str) -> str:
+    items = json.loads(os.getenv(env_key))
+    index = randint(0, len(items) - 1)
+    return items[index]
+
+
+def goodbye_text():
+    return get_random_item('farewells')
+
+
+def welcome_text():
+    start = get_random_item('onboards')
+    middle = ' Reply with a city of your dreams. Example:\nLOCATION '
+    end = get_random_item('cities')
+    return start + middle + end
+
+
+def city_not_found_text():
+    return get_random_item('not_found') + get_random_item('cities')
+
+
+def main():
+    print(city_not_found_text())
+
+
+
+
+if __name__ == '__main__':
+    main()
--- modules/user_interaction.py
+++ modules/user_interaction.py
@@ -1,44 +1,54 @@
 from dotenv import load_dotenv
 from datetime import datetime
+import json
 import os
+from random import randint
+import requests
 
 from modules.messages_manager import register_number, unregister_number, read_messages, send_message
+from modules.sms_builder import welcome_text, goodbye_text, city_not_found_text
 
 load_dotenv()
 some_phone_number = int(os.getenv('some_number'))
 sms_commands = {}
 TEAM_NAME = 'Attraction'
+LOG_FILENAME = os.path.join('..', 'storage', 'app.log')
+DEV_LOG = 'app.log'
 
 
-def add_log_record(status: int, record: str, log_filename: str = os.path.join('..', 'storage', 'app.log')):
-    with open(log_filename, 'a', encoding='utf8') as log:
+def add_log_record(status: int, record: str, log_file: str = DEV_LOG):
+    with open(log_file, 'a', encoding='utf8') as log:
         log.write(f'{datetime.now()}\t{status} {record}\n')
 
 
 def subscribe_user(user_number: int, text: str = '', team: str = TEAM_NAME) -> int:
-    add_log_record(100, f'Subscribing number {user_number} to {text} group')
+    if text:
+        text = f' ({text})'
+    add_log_record(100, f'Subscribing number {user_number} to {team} group{text}')
     code, message = register_number(user_number)
     if code == 200:
-        add_log_record(code, f'User {user_number} was successfully subscribed for {team}')
-        loc_keyword = list(sms_commands.keys())[2]
-        welcome_text = ('Welcome to the DiscoverSphere app!\n'
-                        f'To set your destination reply to this SMS with keyword {loc_keyword} '
-                        f'followed by a city. Example:\n{loc_keyword} Berlin')
-        sms_code, sms_message = send_message(user_number, welcome_text)
-        add_log_record(sms_code, sms_message)
+        add_log_record(code, f'User {user_number} was successfully subscribed to {team}')
+        sms_text = welcome_text()
+        # sms_code, sms_message = send_message(user_number, sms_text)
+        # add_log_record(sms_code, sms_message)
         # TODO add new user to persistent storage
-        return sms_code
+        # return sms_code
     add_log_record(code, message)
     return code
 
 
-def unsubscribe_user(user_number: int, text: str, team: str = TEAM_NAME) -> int:
-    add_log_record(100, f'Unsubscribing {user_number} from {team} ({text})')
+def unsubscribe_user(user_number: int, text: str = '', team: str = TEAM_NAME) -> int:
+    if text:
+        text = f' ({text})'
+    add_log_record(100, f'Unsubscribing {user_number} from "{team}"{text}')
     code, message = unregister_number(user_number)
     add_log_record(code, message)
     if code == 200:
-        # TODO send unsubscribe confirmation sms
-        return code
+        sms_text = goodbye_text()
+        # sms_code, sms_message = send_message(user_number, sms_text)
+        # add_log_record(sms_code, sms_message)
+        # return sms_code
+    add_log_record(code, message)
     return code
 
 
@@ -63,17 +73,21 @@ def send_documentation():
 
 
 sms_commands.update({
-    'SUBSCRIBE': ['SUBSCRIBE', subscribe_user],
-    'UNSUBSCRIBE': ['UNSUBSCRIBE', unsubscribe_user],
-    'LOCATION': ['LOCATION', set_location],
-    'TYPE': ['TYPE', set_attraction_type],
-    'MORE': ['MORE', send_next_attraction],
-    'DOCS': ['DOCS', send_documentation]
+    'SUBSCRIBE': subscribe_user,
+    'UNSUBSCRIBE': unsubscribe_user,
+    'LOCATION': set_location,
+    'TYPE': set_attraction_type,
+    'MORE': send_next_attraction,
+    'DOCS': send_documentation
 })
+
+
+
 
 
 def main():
     subscribe_user(some_phone_number)
+    unsubscribe_user(some_phone_number)
 
 
 if __name__ == '__main__':
