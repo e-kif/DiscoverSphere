40c05971039dd0091a2875d6dd73847fc8bfe203
e-kif <e.kifiuk@gmail.com>
2024-11-28 18:23:10 +0200

    storage_users was initiated

---
+++ modules/storage_users.py
@@ -0,0 +1,12 @@
+"""Module interacts with user persistence storage"""
+
+
+# todo check if a storage file exists
+
+# todo initiate storage file
+
+# todo check if a storage file empty
+
+# todo user init (phone number: int, location: str, type: str, places: list, index: int)
+
+# todo setters and getters for location, type, places, index
