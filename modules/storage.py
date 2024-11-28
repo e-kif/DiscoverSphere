c17d11670d39c51e11a5144b2bb73c36c5a4f435
Tyberos2331 <jamestorras@gmail.com>
2024-11-28 10:22:52 +0100

    hello this is the storage thingy

--- modules/storage.py
+++ modules/storage.py
@@ -7,3 +7,26 @@ we should save
 - list of attractions
 - last attraction that we sent to the user
 """
+
+class UserTracker:
+    def __init__(self):
+        self.users = {}
+
+    def add_user(self, phone_number):
+        if phone_number not in self.users:
+            self.users[phone_number] = {
+                'last_location': None,
+                'last_attraction_type': None,
+                'attractions': [],
+                'last_sent_attraction': None
+            }
+
+    def update_user_info(self, phone_number, **kwargs):
+        if phone_number in self.users:
+            self.users[phone_number].update(kwargs)
+
+    def get_user_info(self, phone_number):
+        if phone_number in self.users:
+            return self.users[phone_number]
+        else:
+            return None
