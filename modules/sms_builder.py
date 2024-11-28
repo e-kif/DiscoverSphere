927c7ffb590a5e64c92451320ddbcbb989a45672
e-kif <e.kifiuk@gmail.com>
2024-11-28 14:00:30 +0200

    url shortener implemented

--- modules/sms_builder.py
+++ modules/sms_builder.py
@@ -2,15 +2,43 @@ import os
 from dotenv import load_dotenv
 import json
 from random import randint
+import requests
 
 
 load_dotenv()
+ATTRACTIONS_LONG = 'https://raw.githubusercontent.com/e-kif/DiscoverSphere/refs/heads/main/static/attractions_types.json'
+ATTRACTIONS = 'https://tinyurl.com/2yyxqodb'
 
 
 def get_random_item(env_key: str) -> str:
     items = json.loads(os.getenv(env_key))
     index = randint(0, len(items) - 1)
     return items[index]
+
+
+def get_attractions_list(storage: str = os.path.join('static', 'attractions_types.json')) -> list:
+    with open(storage, 'r', encoding='utf8') as file:
+        attractions = json.loads(file.read())
+    all_attractions = []
+    for key, value in attractions.items():
+        all_attractions += [key] + value
+    return all_attractions
+
+
+def get_random_attraction_type():
+    attractions = get_attractions_list()
+    return attractions[randint(0, len(attractions) - 1)]
+
+
+def make_url_short(url: str):
+    request_url = 'https://tinyurl.com/api-create.php?url=' + url
+    try:
+        response = requests.get(request_url)
+    except Exception:
+        return url
+    if response.status_code != 200:
+        return url
+    return response.text
 
 
 def goodbye_text():
@@ -28,10 +56,14 @@ def city_not_found_text():
     return get_random_item('not_found') + get_random_item('cities')
 
 
+def attraction_type_text(location):
+    type1 = get_random_attraction_type()
+    type2 = get_random_attraction_type()
+
+
+
 def main():
-    print(city_not_found_text())
-
-
+    pass
 
 
 if __name__ == '__main__':
