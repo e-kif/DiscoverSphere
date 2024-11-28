64f10684ef83f339016a4955c4689b594f62cc7d
e-kif <e.kifiuk@gmail.com>
2024-11-28 18:16:48 +0200

    debug switch implemented in user_interaction, set_location implementation is in progress

--- modules/user_interaction.py
+++ modules/user_interaction.py
@@ -3,8 +3,9 @@ from datetime import datetime
 import os
 
 from modules.messages_manager import register_number, unregister_number, read_messages, send_message
-from modules.sms_builder import welcome_text, goodbye_text, city_not_found_text
+from modules.sms_builder import welcome_text, goodbye_text, city_not_found_text, attraction_type_text
 from modules.storage_manager import save_message, get_all_messages
+from modules.attractions import geocode_city_finder
 
 load_dotenv()
 some_phone_number = int(os.getenv('some_number'))
@@ -13,6 +14,7 @@ TEAM_NAME = 'Attraction'
 LOG_FILENAME = os.path.join('..', 'storage', 'app.log')
 DEV_LOG = 'app.log'
 SEND_SMS = False
+DEBUG = True
 
 
 def add_log_record(status: int, record: str, log_file: str = DEV_LOG):
@@ -23,6 +25,7 @@ def add_log_record(status: int, record: str, log_file: str = DEV_LOG):
 
 def obfuscate(func):
     """Function decorator that obfuscates part of the user's phone number for logs"""
+
     def inner(user_number, text='', team=TEAM_NAME):
         obfuscated_user_number = (len(str(user_number)) - 4) * '*' + str(user_number)[-4:]
         return func(user_number, obfuscated_user_number, text, team)
@@ -44,7 +47,7 @@ def str_to_datetime(text: str) -> datetime:
     return datetime.fromisoformat(text.split('+')[0])
 
 
-def get_new_messages(old: dict, new: dict) -> list:
+def filter_new_messages(old: dict, new: dict) -> list:
     """Takes two dicts. Returns a list of messages that are present in new messages
     and are absent in old ones. Logs amount of returned messages.
     Overwrites messages' storage
@@ -73,10 +76,9 @@ def process_new_message(message: dict, commands: dict = sms_commands):
             if data['text'].startswith(command):
                 command, *text = data['text'].strip().split(' ')
                 text = ' '.join(text)
-                return commands.get(command, commands['DOCS'])(user_number, text)
+                return commands.get(command, commands['DOCS'])(user_number=user_number, text=text)
         text = data['text'].strip()
-        print(f'{text=}')
-        return commands['DOCS'](user_number, text)
+        return commands['DOCS'](user_number=user_number, text=text)
 
 
 @obfuscate
@@ -84,12 +86,14 @@ def subscribe_user(user_number: int, obfuscated_number: str = '', text: str = ''
     """Adds user to a team group, adds user info to a storage,
     sends welcome sms that asks for location, adds some logs.
     """
+    if DEBUG: print(f'subscribing {user_number} to {team} ({text=})')
     if text:
+        team = text
         text = f' ({text})'
     if not obfuscated_number:
         obfuscated_number = str(user_number)
     add_log_record(100, f'Subscribing number {obfuscated_number} to {team} group{text}')
-    code, message = register_number(user_number)
+    code, message = register_number(user_number) # todo add a team as parameter
     if code == 200:
         add_log_record(code, f'User {user_number} was successfully subscribed to {team}')
         # TODO add new user to persistent storage (set location, type, index to none)
@@ -105,12 +109,14 @@ def subscribe_user(user_number: int, obfuscated_number: str = '', text: str = ''
 @obfuscate
 def unsubscribe_user(user_number: int, obfuscated_number: str = '', text: str = '', team: str = TEAM_NAME) -> int:
     """Unregisters user from a team group, sends farewell sms, adds logs"""
+    if DEBUG: print(f'removing {user_number} from {team}')
     if text:
+        team = text
         text = f' ({text})'
     if not obfuscated_number:
         obfuscated_number = user_number
     add_log_record(100, f'Unsubscribing {obfuscated_number} from "{team}"{text}')
-    code, message = unregister_number(user_number)
+    code, message = unregister_number(user_number) # todo add a team parameter
     add_log_record(code, message)
     if code == 200:
         if SEND_SMS:
@@ -126,16 +132,24 @@ def set_location(user_number: int,
                  text: str = '',
                  team: str = TEAM_NAME) -> (int, str):
     """Checks if location exists, if not - send sms. Saves location to a storage. Sends """
-    # todo if location exists (send message if not)
+    if DEBUG: print(f'this should set location to {text} for {user_number} ({text=})')
+    coords = geocode_city_finder(text, os.getenv('GEOAPIFY_API_KEY'))
+    if not coords:
+        sms_text = city_not_found_text()
+        if DEBUG: print(sms_text)
+        if SEND_SMS:
+            sms_code, sms_message = send_message(user_number, sms_text)
+            return f'400 {sms_code}', f'City not found. {sms_message}'
     # todo save location info to a storage
     # todo empty type and index in storage
-    # todo generate sms request for types
+    sms_text = attraction_type_text(text)
+    if DEBUG: print(f'sending message: {sms_text}')
+    if SEND_SMS:
+        sms_code, sms_message = send_message(user_number, sms_text)
+        return sms_code, sms_message
+
     # todo log the process
-
-    # code, message = send_message(user_number, sms_text)
-    # return code, message
-
-    pass
+    return coords
 
 
 def set_attraction_type(user_number: int,
@@ -145,6 +159,7 @@ def set_attraction_type(user_number: int,
     """Validates attractions type (if not valid - send sms). Saves attraction type to a storage.
     Fetches and a list of attractions. If type is not 'surprise' saves a list of attractions to a storage.
     Sends attraction info to the user. Logs the process."""
+    if DEBUG: print(f'this should set attraction type to {text} for {user_number} ({text=})')
     # todo validate attraction type (send sms if false)
     # todo write type to a storage
     # todo read location from storage
@@ -162,6 +177,7 @@ def send_next_attraction(user_number: int,
                          text: str = '',
                          team: str = TEAM_NAME) -> (int, str):
     """Gets user info from a storage, sends nex attraction that corresponds to previous user's requests"""
+    if DEBUG: print(f'this should send next attraction to {user_number} ({text=})')
     # todo read location info from a storage
     # todo read type info from a storage
     # todo read index from a storage
@@ -177,6 +193,7 @@ def send_documentation(user_number: int,
                        text: str = '',
                        team: str = TEAM_NAME) -> (int, str):
     """Sends sms to a user with available commands"""
+    if DEBUG: print(f'this should send docs to {user_number} ({text=})')
     # todo generate/read text
     # todo send sms
     # todo log the process
@@ -195,13 +212,17 @@ sms_commands.update({
 
 def main():
     """Function for testing module's functions without importing them"""
+
+    test_messages = [{'123': {'text': 'LOCATION Frankfurt', 'receivedAt': '2024-11-27T09:32:59.322+0000'}}]
+    test_message = test_messages[0]
     # subscribe_user(some_phone_number)
     # unsubscribe_user(some_phone_number)
-    code, messages = read_messages('TeamABC')
-    code, messages = read_messages('WaterProof')
-    old = get_all_messages()
-    new_messages = get_new_messages(old, messages)
-    print(new_messages)
+    # code1, messages1 = read_messages('TeamABC')
+    # code2, messages2 = read_messages('WaterProof')
+    coords = process_new_message(test_message)
+    print(f'{coords=}')
+
+
     pass
 
 
